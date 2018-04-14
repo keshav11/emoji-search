ae49b6a36ceffa7bcd5e3a82ca2a7b93c4109075
Keshav Gupta <keshav1032@gmail.com>
2018-04-13 22:36:15 -0400

    added options

--- README.md
+++ README.md
@@ -2,9 +2,26 @@
 search emojis using command line
 
 ## Usage
+### show results where keyword exits anywhere in the emoji name
 ```
-$ python empoji_seach.py grinning_cat_face
+$ python emoji_search.py grinning_cat_face
 
 :grinning_cat_face:  😺
 :grinning_cat_face_with_smiling_eyes:  😸
+```
+### show results only if whole emoji name mathces with keyword
+```
+$ python emoji_search.py -w grinning_cat_face
+
+:grinning_cat_face:  😺
+```
+
+### search and show results ony if full emoji name starts with search keyword
+```
+$ python emoji_search.py -s cat
+
+:cat:  🐈
+:cat_face:  🐱
+:cat_face_with_tears_of_joy:  😹
+:cat_face_with_wry_smile:  😼
 ```
--- source/emoji_search.py
+++ source/emoji_search.py
@@ -1,12 +1,51 @@
 import emoji
 import sys
+import getopt
 
-if __name__ == '__main__':
-    search_key = sys.argv[1]
-    results = {key: emoji.EMOJI_UNICODE[key] for key in emoji.EMOJI_UNICODE.keys()
-               if search_key.lower() in key.lower()}
+
+def print_usage():
+    """print usage of the utility"""
+    print('python', sys.argv[0], '-[sw] search_keyword')
+
+
+def main():
+    # parse and validate options and argument
+    try:
+        options, arguments = getopt.getopt(sys.argv[1:], "sw")
+    except getopt.GetoptError as exp:
+        print(exp)
+        print_usage()
+        sys.exit(-2)
+
+    if len(arguments) == 0:
+        print('search keyword not provided')
+        print_usage()
+        sys.exit(-2)
+
+    starts_with = False
+    whole_word = False
+    for opt, ars in options:
+        if opt == '-s':
+            starts_with = True
+        if opt == '-w':
+            whole_word = True
+
+    search_key = arguments[0]
+    # search for emoji
+    if whole_word:
+        results = {key: emoji.EMOJI_UNICODE[key] for key in emoji.EMOJI_UNICODE.keys() if
+                   ":"+search_key.lower()+":" == key.lower()}
+    elif starts_with:
+        results = {key: emoji.EMOJI_UNICODE[key] for key in emoji.EMOJI_UNICODE.keys() if
+                   key.lower().startswith(':' + search_key)}
+    else:
+        results = {key: emoji.EMOJI_UNICODE[key] for key in emoji.EMOJI_UNICODE.keys() if
+                   search_key.lower() in key.lower()}
+
+    # print out results
     for key, val in results.items():
         print(key + "  " + val)
 
 
-
+if __name__ == '__main__':
+    main()
